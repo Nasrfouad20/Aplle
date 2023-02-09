c277401ed2f93718ccc4ee606fdde2c7129c6449
Nasr Fouad <123129891+Nasrfouad20@users.noreply.github.com>
2023-02-10 00:25:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let contener = document.querySelector('.contener');
+function phones(phone){
+    img.src = phone;;
+
+}
+function colors(color){
+    contener.style.backgroundColor = color;
+
+}
